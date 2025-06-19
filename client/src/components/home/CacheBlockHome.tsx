847a3318a99a331a0c46dec8539f5e1f10eca270
joewang0430 <joewj0430@gmail.com>
2025-06-19 04:19:38 -0400

    basic local storage module finished

--- client/src/components/home/CacheBlockHome.tsx
+++ client/src/components/home/CacheBlockHome.tsx
@@ -1,10 +1,10 @@
-import CacheUpload from "../setup/ui/CacheUpload";
+// import CacheUpload from "../setup/ui/CacheUpload";
 
 const CacheBlockHome = () => {
   return (
     <div>
       <h2 className="text-green-400">CacheBlockHome</h2>
-      <CacheUpload />
+      {/* <CacheUpload /> */}
     </div>
   );
 }
