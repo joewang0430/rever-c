7ab94044dde3381f6c7d8121e76631861d5a1b42
joewang0430 <joewj0430@gmail.com>
2025-06-22 01:14:29 -0400

    added the archive code community json

--- client/src/data/types/setup.ts
+++ client/src/data/types/setup.ts
@@ -31,3 +31,13 @@ export interface PlayerConfig {
 export type PlayerType = "custom" | "archive" | "human" | "ai" | null; 
 export type CustomType = "candidate" | "cache";
 export type BoardSize = 6 | 8 | 12;
+
+/*
+id: string;
+group: // which group the archive belongs to (file below archives/ in backend)
+name: // full name when being played
+shortName: // short name when showing in the selection list
+description?:    // info shown when being played
+shortDescription?: //  info shown in the selection list
+picture: // path to the picture of each archive selection in setup page
+*/
