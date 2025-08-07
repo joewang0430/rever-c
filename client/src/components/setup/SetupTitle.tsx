568fe8e8581457a91c754540b3d55e9f5eb13fe7
joewang0430 <joewj0430@gmail.com>
2025-08-07 12:42:18 -0400

    setup title basically done

--- client/src/components/setup/SetupTitle.tsx
+++ client/src/components/setup/SetupTitle.tsx
@@ -1,6 +1,29 @@
+import Image from 'next/image';
+
 const SetupTitle = () => {
     return (
-        <>SetupTitle</>
+        <div className="flex justify-center items-center gap-5">
+            {/* Left Side: First Move */}
+            <Image
+                src="/svgs/setup/setup_black.svg"
+                alt="First Move"
+                width={112} 
+                height={39}
+            />
+
+            {/* Separator */}
+            <span className="text-2xl text-gray-500 rvct-theme-500">
+                VS
+            </span>
+
+            {/* Right Side: Move After */}
+            <Image
+                src="/svgs/setup/setup_white.svg"
+                alt="Move After"
+                width={112}
+                height={39}
+            />
+        </div>
     );
 };
 
